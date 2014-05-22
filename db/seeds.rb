7d56e51fbe3ff60e9c148e497c482c774ba2a63c
vagrant <vagrant@rails-dev-box>
2014-05-22 17:15:20 +0000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,25 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
 #Create Posts
 50.times do
   Post.create(
@@ -16,6 +36,13 @@ posts = Post.all
     body: Faker::Lorem.paragraph
   )
 end
+
+# db/seeds.rb
+
+User.first.update_attributes(
+  email: 'youremail.com',
+  password: 'helloworld',
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
