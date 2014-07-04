f1bdb62fdd577a8610851216d10cff5c7f40caf5
vagrant <vagrant@rails-dev-box>
2014-07-04 16:38:54 +0000

    Added image to Post.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+  mount_uploader :avatar, AvatarUploader
+
+
+  def role?(base_role)
+    role == base_role.to_s
+  end
 end
 
-def role?(base_role)
-  role == base_role.to_s
-end
