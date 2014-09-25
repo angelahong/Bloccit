e01052981f5d54bb84b36cb03dac5f1d075a9682
vagrant <vagrant@rails-dev-box>
2014-09-25 16:17:29 +0000

    Created private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,5 @@ class Topic < ActiveRecord::Base
   scope :visible_to, -> (user) { user ? all : where(public: true) }
   scope :publicly_viewable, -> { where(public: true) }
   scope :privately_viewable, -> { where(public: false) }
+  #add new info
 end
